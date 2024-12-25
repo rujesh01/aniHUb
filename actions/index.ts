b31b4f279e313cf2fc350cf3795293a162372cfb
rujesh01 <rujesh2021@gmail.com>
2024-12-25 22:05:45 +0545

    added some extra files

---
+++ actions/index.ts
@@ -0,0 +1,21 @@
+"use server";
+
+const url = process.env.BASE_URL;
+
+console.log(url);
+
+const GetAnime = async () => {
+  try {
+    const res = await fetch(`${url}//api/v2/hianime/home`);
+
+    if (!res.ok) {
+      return null;
+    }
+
+    return await res.json();
+  } catch (error) {
+    return null;
+  }
+};
+
+export default GetAnime;
