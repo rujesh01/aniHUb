15824eb516b24b5c879583c4c4c4009ce34463ba
rujesh01 <rujesh2021@gmail.com>
2024-12-25 21:49:57 +0545

    added auth5 and prisma

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
+ 
+
+const { auth } = NextAuth(authConfig)
+
+export default auth(async function middleware(req) {
+
+const isLogin = req.auth
+
+console.log(isLogin)
+
+  // Your custom middleware logic goes here
+})
+
+
+export const config = {
+    matcher: [
+      // Skip Next.js internals and all static files, unless found in search params
+      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+      // Always run for API routes
+      '/(api|trpc)(.*)',
+    ],
+  }
