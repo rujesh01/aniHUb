287985f8aaf7736274460e36da18879901c86df5
Anup Maharjan <anupmhrzn16@gmail.com>
2025-01-09 17:33:39 +0545

    build:add yarn.loc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { "@typescript-eslint/no-unused-vars": "off" },
 ];
 
 export default eslintConfig;
