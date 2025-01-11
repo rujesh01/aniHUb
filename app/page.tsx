dd41f76184661c8143be982bbf01753eb005885c
rujesh01 <rujesh2021@gmail.com>
2025-01-11 16:21:17 +0545

    o

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,9 @@ export default async function Home() {
   return (
     <div>
       <section>
-<<<<<<< HEAD
-        <h1>This is root page</h1>
-        <section>
-          <AniHubLogo />
-        </section>
-        <Link href={"/home"}>
-          <Button className="flex">Go to home</Button>
-        </Link>
-        <Link href={"/watch"}>
-          <Button className="flex">Go to watch</Button>
-        </Link>
-=======
         <Navbar />
         <Header />
         <AnimeDescription />
->>>>>>> arjun
       </section>
     </div>
   );
