795635d2200dd69d956a7a63b3a0e45a76a9bcc1
rujesh01 <rujesh2021@gmail.com>
2025-01-20 21:17:17 +0545

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Navbar from "@/components/LandingPage/Navbar";
 import Header from "@/components/LandingPage/SearchAnime";
 import AnimeDescription from "@/components/LandingPage/Description";
-import AniHubLogo from "@/components/Logo";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
